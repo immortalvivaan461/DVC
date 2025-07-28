2fad886390eb8a2df6285a4401a4a771bdeffe07
Vivaan Saroj <149756428+immortalvivaan461@users.noreply.github.com>
2025-07-29 04:01:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const path = require("path");
 const { error } = require("console");
 const PORT = process.env.PORT || 3000;
 
-require("dotenv").config();
-await mongoose.connect(process.env.MONGO_URI);
+
+
 
 app.use(express.urlencoded({ extended: true }));
 app.set("views", path.join(__dirname, "views"));
@@ -14,7 +14,11 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 const mongoose = require('mongoose');
+require("dotenv").config();
+
 const COSTUMER = require("./models/costumer.js")
+await mongoose.connect(process.env.MONGO_URI);
+
 
 
 // main()
